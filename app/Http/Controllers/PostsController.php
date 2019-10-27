35aee41d34b421118ca2493290ef5e45a1281ad9
Vladimir <56157404+VladimirZ1@users.noreply.github.com>
2019-10-27 18:54:22 +0300

    1

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Post;
 
 class PostsController extends Controller
 {
@@ -23,6 +24,11 @@ class PostsController extends Controller
      */
     public function index()
     {
+        $post = Post::create([
+                'title' => 'title',
+                'post'  => 'Post'
+            ]);
         return view('posts');
     }
+
 }
